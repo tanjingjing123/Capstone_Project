a05fc522d69883bf7bff31030fef0efbff1abb4c
Jane Tan <jane@Janes-MacBook-Pro.local>
2021-12-19 23:19:56 -0800

    add ssl key

--- Emotion Recognizer/backend/bootstrap.sh
+++ Emotion Recognizer/backend/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export FLASK_APP=./controller.py
 source $(pipenv --venv)/bin/activate
-flask run -h 0.0.0.0 -p 8020 --cert=adhoc
+flask run -h 0.0.0.0 -p 8020 --cert="cert.pem" --key="key.pem"
